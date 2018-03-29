362b477d04c067ac50f1573dc167bbbcf34028b7
Yang Sujin <21500394@handong.edu>
2018-03-29 11:02:13 +0900

    add java doc

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -1,9 +1,18 @@
 package edu.handong.csee.java.lab06;
 import java.util.Scanner;
+/**
+ * This class count number of weeks until the room is full with roachs.
+ * The growth rate and volume of roach increase each week. 
+ * Growth rate increase 0.95 and volume increase 0.002.
+ * HowMuchBug method add growth and volume. And print the result.
+ * @author magic
+ *
+ */
 public class BugInfestation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
